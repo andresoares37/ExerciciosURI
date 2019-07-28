ed2aca6ccf1ffb738ae86a7426647b6d2d1983ca
Andre Soares <andresoares37@gmail.com>
2019-07-28 19:49:48 -0300

    commit

---
+++ EstruturaWhile/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace EstruturaWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split(' ');
+
+            int X = int.Parse(valores[0]);
+
+            int Y = int.Parse(valores[1]);
+
+            while (X != Y)
+            {
+                if (X < Y)
+                {
+                    Console.WriteLine("Crescente");
+                }
+                else
+                {
+                    Console.WriteLine("Descrescente");
+                }
+
+                valores = Console.ReadLine().Split(' ');
+
+                X = int.Parse(valores[0]);
+
+                Y = int.Parse(valores[1]);
+            }
+        }
+    }
+}
